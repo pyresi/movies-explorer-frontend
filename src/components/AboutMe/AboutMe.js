7a9d1774127459b8ca3549889c6c227a9ccb16cb
pyresi <pyresi@yandex.ru>
2023-09-16 20:52:42 +0300

    Add main page

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,8 +1,46 @@
+import './AboutMe.css';
+import './AboutMe__name.css';
+import './AboutMe__bio.css';
+import './AboutMe__description.css';
+import './AboutMe__box.css';
+import './AboutMe__portrait.css';
+import './AboutMe__github.css';
+import './AboutMe__portfolio.css';
+import './AboutMe__link.css';
+import './AboutMe__links.css';
+import './AboutMe__link-container.css';
+import './AboutMe__link-arrow.css';
+import portrait from '../../images/portrait.png';
+
 function AboutMe() {
     return (
-        <>
-            <p>About me</p>
-        </>
+        <div className="aboutme">
+            <h2 className='aboutproject__header'>Студент</h2>
+            <hr className='aboutproject__line'></hr>
+            <div className='aboutme__box'>
+                <h2 className='aboutme__name'>Анастасия</h2>
+                <p className='aboutme__bio'>Фронтенд-разработчик, 25 лет</p>
+                <p className='aboutme__description'>Закончила РУДН по специальности лингвистика, работала переводчиком пока не прошла курс по веб-разработке. Сейчас живу в Москве. В свободное время занимаюсь волонтерством и спортом.
+                </p>
+                <img src={portrait} alt="Портретная фотография студента" className='aboutme__portrait'></img>
+                <a className='aboutme__github' href='#'>GitHub</a>
+                <a className='aboutme__portfolio' href='#'>Портфолио</a>
+                <ul className='aboutme__links'>
+                    <li className='aboutme__link-container'>
+                        <a className='aboutme__link' href='#'>Статичный сайт</a>
+                        <button className='aboutme__link-arrow' type='button'></button>
+                    </li>
+                    <li className='aboutme__link-container'>
+                        <a className='aboutme__link' href='#'>Адаптивный сайт</a>
+                        <button className='aboutme__link-arrow' type='button'></button>
+                    </li>
+                    <li className='aboutme__link-container'>
+                        <a className='aboutme__link' href='#'>Одностраничное приложение</a>
+                        <button className='aboutme__link-arrow' type='button'></button>
+                    </li>
+                </ul>
+            </div>
+        </div >
     );
 }
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,20 @@
+import './Footer.css';
+import './Footer__paragraph.css';
+import './Footer__box.css';
+import './Footer__box-element.css';
+import './Footer__links.css';
+
 function Footer() {
     return (
         <footer className="footer">
-            <p className="footer__copyright">© 2023</p>
+            <p className="footer__paragraph">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+            <div className="footer__box">
+                <p className="footer__box-element">© 2023</p>
+                <div className='footer__links'>
+                    <p className="footer__box-element">Яндекс.Практикум</p>
+                    <p className="footer__box-element">GitHub</p>
+                </div>
+            </div>
         </footer>
     );
 }
